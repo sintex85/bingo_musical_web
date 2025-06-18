ceea9ab9ee15b9168a67276a4f831b39c04ae788
Paco <kitortola@gmail.com>
2025-06-18 12:41:06 +0200

    s

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const SpotifyWebApi = require('spotify-web-api-node');
 const crypto = require('crypto');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3333;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
